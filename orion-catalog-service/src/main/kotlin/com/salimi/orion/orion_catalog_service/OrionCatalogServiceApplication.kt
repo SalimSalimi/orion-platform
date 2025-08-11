f2700f2a4e7eba70f65b962563ddb62c9a35a600
Salim Salimi <salim.salimi@protonmail.com>
2025-08-11 10:37:31 +0200

    init catalog service

---
+++ orion-catalog-service/src/main/kotlin/com/salimi/orion/orion_catalog_service/OrionCatalogServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.salimi.orion.orion_catalog_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrionCatalogServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrionCatalogServiceApplication>(*args)
+}
---
+++ orion-catalog-service/src/test/kotlin/com/salimi/orion/orion_catalog_service/OrionCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.salimi.orion.orion_catalog_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrionCatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
