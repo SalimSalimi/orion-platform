bf1385d2b50807ac1152780f2584c1ef0205b79a
Salim Salimi <salim.salimi@protonmail.com>
2025-08-12 16:48:29 +0200

    catalog-bff: initialisation of bff for catalog

---
+++ catalog-bff/src/main/kotlin/orion/catalog/catalog_bff/CatalogBffApplication.kt
@@ -0,0 +1,11 @@
+package orion.catalog.catalog_bff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogBffApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogBffApplication>(*args)
+}
---
+++ catalog-bff/src/test/kotlin/orion/catalog/catalog_bff/CatalogBffApplicationTests.kt
@@ -0,0 +1,13 @@
+package orion.catalog.catalog_bff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatalogBffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
