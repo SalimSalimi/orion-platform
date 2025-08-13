12525a18e2b08279f17b6da3a58ecfd3697b12b4
Salim Salimi <salim.salimi@protonmail.com>
2025-08-13 16:30:24 +0200

    catalog-bff: adding token for every request of catalog api using client credentials

--- catalog-bff/src/main/kotlin/orion/catalog/catalog_bff/infrastructure/api/catalogservice/RestClientApiConfig.kt
+++ catalog-bff/src/main/kotlin/orion/catalog/catalog_bff/infrastructure/api/catalogservice/RestClientApiConfig.kt
@@ -4,14 +4,27 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor
 import org.springframework.web.client.RestClient
 
+
 @Configuration
-class RestClientApiConfig {
+class RestClientApiConfig(
+    private val authorizedClientManager: OAuth2AuthorizedClientManager
+) {
 
     @Bean
     @Qualifier("catalogApiRestClient")
-    fun restClient(builder: RestClient.Builder,
-                   @Value("\${api.catalog.base-url}") baseUrl: String) =
-        builder.baseUrl(baseUrl).build()
+    fun restClient(builder: RestClient.Builder, @Value("\${api.catalog.base-url}") baseUrl: String): RestClient {
+        val requestInterceptor =
+            OAuth2ClientHttpRequestInterceptor(authorizedClientManager)
+
+        requestInterceptor.setClientRegistrationIdResolver { request -> "catalog-bff-credentials" }
+
+        return builder
+            .baseUrl(baseUrl)
+            .requestInterceptor(requestInterceptor)
+            .build()
+    }
 }
