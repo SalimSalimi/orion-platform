6b18a5ae3ba1fd33df25e5958f17f9caed21dade
Salim Salimi <salim.salimi@protonmail.com>
2025-08-28 10:48:52 +0200

    catalog-bff: using parallel execution => cut execution time by half!

--- catalog-bff/src/main/kotlin/orion/catalog/catalog_bff/application/products/impl/GetAllProductsWithCategoriesUsecaseImpl.kt
+++ catalog-bff/src/main/kotlin/orion/catalog/catalog_bff/application/products/impl/GetAllProductsWithCategoriesUsecaseImpl.kt
@@ -1,20 +1,25 @@
 package orion.catalog.catalog_bff.application.products.impl
 
+import org.slf4j.LoggerFactory
 import orion.catalog.catalog_bff.application.categories.GetCategoryByIdUsecase
 import orion.catalog.catalog_bff.application.products.GetAllProductsWithCategoriesUsecase
 import orion.catalog.catalog_bff.application.products.dto.CategoryDto
 import orion.catalog.catalog_bff.application.products.dto.ProductWithCategoryDto
 import orion.catalog.catalog_bff.application.products.repository.ProductRepository
+import orion.catalog.catalog_bff.application.shared.ParallelUsecaseExecutor
 import orion.catalog.catalog_bff.application.shared.annotation.Usecase
 import orion.catalog.catalog_bff.domain.category.Category
 import orion.catalog.catalog_bff.domain.product.Product
-import orion.catalog.catalog_bff.infrastructure.helpers.MultiThreadUsecaseExecutorImpl
+import orion.catalog.catalog_bff.infrastructure.executors.MultiThreadUsecaseExecutorImpl
+import kotlin.system.measureTimeMillis
 
 @Usecase
 class GetAllProductsWithCategoriesUsecaseImpl(
     private val productRepository: ProductRepository,
-    private val getCategoryByIdUsecase: GetCategoryByIdUsecase
+    private val getCategoryByIdUsecase: GetCategoryByIdUsecase,
+    private val parallelUsecaseExecutor: ParallelUsecaseExecutor,
 ): GetAllProductsWithCategoriesUsecase {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     override fun execute(input: Unit): List<ProductWithCategoryDto> {
         val products = productRepository.getAll()
@@ -23,7 +28,11 @@ class GetAllProductsWithCategoriesUsecaseImpl(
         if (products.isNotEmpty()) {
             products.forEach { product ->
                 run {
-                    val categories = getCategories(product.categoriesId)
+                    var categories: Set<Category> = HashSet()
+                    val duration = measureTimeMillis {
+                        categories = getCategoriesInParallel(product.categoriesId)
+                    }
+                    logger.info("Execution time of usecase: $duration ms")
                     result.add(mapProductWithCategories(product, categories))
                 }
             }
@@ -45,6 +54,15 @@ class GetAllProductsWithCategoriesUsecaseImpl(
         }
         return result
     }
+
+    private fun getCategoriesInParallel(categoriesId: Set<String>): Set<Category> {
+        val pairCategories: List<Pair<GetCategoryByIdUsecase, String>> = categoriesId.map { categoryId ->
+            Pair(getCategoryByIdUsecase, categoryId)
+        }
+        @Suppress("UNCHECKED_CAST")
+        return parallelUsecaseExecutor.executeAll(pairCategories).toSet() as Set<Category>
+    }
+
 
     private fun mapProductWithCategories(product: Product, categories: Set<Category>): ProductWithCategoryDto {
         return ProductWithCategoryDto(
